156f8170602586a40d671e5819b3f7506f2001dc
Kevin Caeyman <kevin.caeyman@gmail.com>
2023-01-26 19:42:03 +0100

    first commit

---
+++ js/checkout.js
@@ -0,0 +1,28 @@
+
+// Exercise 6
+function validate() {
+	let error = 0;
+	// Get the input fields
+	let fName = document.getElementById("fName");
+	let fEmail = document.getElementById("fEmail");
+
+	// Get the error elements
+	let errorName = document.getElementById("errorName");
+	let errorEmail = document.getElementById("errorEmail");  
+	
+	// Validate fields entered by the user: name, phone, password, and email
+	if(fName.value == ""){
+		error++;
+	}
+
+	if(fEmail.value == ""){
+		error++;
+	}
+	 
+	if(error>0){
+		alert("Error");
+	}else{
+		alert("OK");
+	}
+
+}
